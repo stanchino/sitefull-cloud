161e60fd5436c203f300fcd1a5ec6f25837de8e4
Stanimir Dimitrov <stanchino@gmail.com>
2016-12-15 10:54:56 +0200

    Updating gems

--- lib/sitefull-cloud/provider/azure/instance.rb
+++ lib/sitefull-cloud/provider/azure/instance.rb
@@ -14,24 +14,18 @@ module Sitefull
         private
 
         def network_interface_setup(subnet, security_group, public_ip, name)
-          ip_configuration_props = NetworkInterfaceIPConfigurationPropertiesFormat.new
-          ip_configuration_props.private_ipallocation_method = 'Dynamic'
-          ip_configuration_props.subnet = subnet
-          ip_configuration_props.public_ipaddress = public_ip
-
           ip_configuration = NetworkInterfaceIPConfiguration.new
           ip_configuration.name = name
-          ip_configuration.properties = ip_configuration_props
-
-          props = NetworkInterfacePropertiesFormat.new
-          props.primary = true
-          props.network_security_group = security_group
-          props.ip_configurations = [ip_configuration]
+          ip_configuration.private_ipallocation_method = 'Dynamic'
+          ip_configuration.subnet = subnet
+          ip_configuration.public_ipaddress = public_ip
 
           network_interface = NetworkInterface.new
           network_interface.location = options[:region]
           network_interface.name = name
-          network_interface.properties = props
+          network_interface.primary = true
+          network_interface.network_security_group = security_group
+          network_interface.ip_configurations = [ip_configuration]
 
           connection.network.network_interfaces.create_or_update(resource_group_name, name, network_interface)
         end
@@ -40,13 +34,9 @@ module Sitefull
           dns_settings = PublicIPAddressDnsSettings.new
           dns_settings.domain_name_label = name
 
-          props = PublicIPAddressPropertiesFormat.new
-          props.public_ipallocation_method = 'Dynamic'
-          props.dns_settings = dns_settings
-
           public_ip = PublicIPAddress.new
+          public_ip.public_ipallocation_method = 'Dynamic'
           public_ip.location = options[:region]
-          public_ip.properties = props
 
           connection.network.public_ipaddresses.create_or_update(resource_group_name, name, public_ip)
         end
@@ -59,14 +49,11 @@ module Sitefull
           storage_account = storage_account(name)
           return storage_account unless storage_account.nil?
 
-          properties = StorageAccountPropertiesCreateParameters.new
-
           sku = Sku.new
           sku.name = 'Standard_LRS'
           sku.tier = 'Standard'
 
           params = StorageAccountCreateParameters.new
-          params.properties = properties
           params.sku = sku
           params.kind = 'Storage'
           params.location = options[:region]
@@ -80,8 +67,7 @@ module Sitefull
 
         def instance_setup(storage, network_interface, instance_data)
           # Create a model for new virtual machine
-          props = VirtualMachineProperties.new
-
+          #
           #windows_config = WindowsConfiguration.new
           #windows_config.provision_vmagent = true
           #windows_config.enable_automatic_updates = true
@@ -102,21 +88,19 @@ module Sitefull
           os_profile.admin_username = instance_data[:key].ssh_user
           os_profile.linux_configuration = linux_config
 
-          props.os_profile = os_profile
 
           hardware_profile = HardwareProfile.new
           hardware_profile.vm_size = instance_data[:machine_type]
-          props.hardware_profile = hardware_profile
-
-          props.storage_profile = create_storage_profile(instance_data[:image], storage.name)
 
           network_profile = NetworkProfile.new
           network_profile.network_interfaces = [network_interface]
-          props.network_profile = network_profile
 
           params = VirtualMachine.new
           params.type = 'Microsoft.Compute/virtualMachines'
-          params.properties = props
+          params.os_profile = os_profile
+          params.network_profile = network_profile
+          params.hardware_profile = hardware_profile
+          params.storage_profile = create_storage_profile(instance_data[:image], storage.name)
           params.location = options[:region]
 
           connection.compute.virtual_machines.create_or_update(resource_group_name, instance_data[:name], params)
--- lib/sitefull-cloud/provider/azure/networking.rb
+++ lib/sitefull-cloud/provider/azure/networking.rb
@@ -8,44 +8,33 @@ module Sitefull
 
         private
         def security_group_setup
-          props = NetworkSecurityGroupPropertiesFormat.new
 
           security_group = NetworkSecurityGroup.new
           security_group.location = options[:region]
-          security_group.properties = props
 
           connection.network.network_security_groups.create_or_update(resource_group_name, SECURITY_GROUP, security_group)
         end
 
         def network_setup(resource_group, security_group)
-          props = VirtualNetworkPropertiesFormat.new
-
           address_space = AddressSpace.new
           address_space.address_prefixes = [NETWORK_CIDR_BLOCK]
-          props.address_space = address_space
-
-          subnet_props = SubnetPropertiesFormat.new
-          subnet_props.address_prefix = SUBNET_CIDR_BLOCK
-          subnet_props.network_security_group = security_group
 
           subnet = Subnet.new
           subnet.name = SUBNET_NAME
-          subnet.properties = subnet_props
-          props.subnets = [subnet]
+          subnet.address_prefix = SUBNET_CIDR_BLOCK
+          subnet.network_security_group = security_group
 
           params = VirtualNetwork.new
           params.location = options[:region]
-          params.properties = props
+          params.address_space = address_space
+          params.subnets = [subnet]
 
           connection.network.virtual_networks.create_or_update(resource_group.name, NETWORK_NAME, params)
         end
 
         def firewall_rule_setup(name, options = {})
-          props = SecurityRulePropertiesFormat.new
-          options.each { |key, value| props.send("#{key}=", value) }
-
           security_rule = SecurityRule.new
-          security_rule.properties = props
+          options.each { |key, value| security_rule.send("#{key}=", value) }
 
           connection.network.security_rules.create_or_update(resource_group_name, SECURITY_GROUP, name, security_rule)
         end
